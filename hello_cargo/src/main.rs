fce5fb4993d421691a6103d1841165e7af8e20c9
Gaëtan <hello@gaetangrond.me>
2025-10-10 22:18:32 +0200

    Initialize main.rs with a basic "Hello, world!" program

---
+++ hello_cargo/src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    // Cargo except file to live inside SRC directory
+    // top-level project directory is just for README files, license information, configuration files, and anything else not related to your code.
+    println!("Hello, world!"); 
+}
---
+++ main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    // ! means we are using a Macro
+    // The main function is special: it is always the first code that runs in every executable Rust program.
+    println!("Hello, world!"); 
+    // rustc main.rs generates a binary, but does not execute it. 
+}
