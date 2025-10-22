2fbe3f81d1d62d511b6268226c59778ee1c569bd
Gaëtan <hello@gaetangrond.me>
2025-10-22 21:07:02 +0200

    Revise structs2 exercise: implement order creation with struct update syntax and enhance test assertions

--- rustlings/exercises/07_structs/structs2.rs
+++ rustlings/exercises/07_structs/structs2.rs
@@ -34,7 +34,8 @@ mod tests {
         let order_template = create_order_template();
 
         // TODO: Create your own order using the update syntax and template above!
-        // let your_order =
+        let your_order = Order{name : "Hacker in Rust".to_string(), count: 1 ,..create_order_template()}; // struct update syntax
+
 
         assert_eq!(your_order.name, "Hacker in Rust");
         assert_eq!(your_order.year, order_template.year);
